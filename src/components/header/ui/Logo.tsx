01d89966b68d58fc928ba45a423aee2d947db118
unknown <d.goncharenko.man@gmail.com>
2024-06-26 07:01:31 +0300

    Added waves to hero

--- src/components/header/ui/Logo.tsx
+++ src/components/header/ui/Logo.tsx
@@ -1,15 +1,13 @@
 import Image from 'next/image'
-import Link from 'next/link'
 import logoLight from '@/assets/icons/logo/logo-light.svg'
 import logoPrimary from '@/assets/icons/logo/logo-primary.svg'
 
 export const Logo = () => {
   return (
-    <div className='relative w-14 h-14 md:w-20 -z-10'>
+    <div className='relative w-14 h-14 md:w-20'>
       <Image
         src={logoLight.src}
         className='md:hidden'
-        color='black'
         fill
         sizes='100%'
         alt='Logo di Pasta La Pepito'
@@ -17,7 +15,6 @@ export const Logo = () => {
       <Image
         src={logoPrimary.src}
         className='hidden md:block'
-        color='black'
         fill
         sizes='100%'
         alt='Logo di Pasta La Pepito'
