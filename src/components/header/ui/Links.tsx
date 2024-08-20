bd1fc38a19c2f3e8f05b2f10b002b550609804d6
unknown <d.goncharenko.man@gmail.com>
2024-08-20 14:15:51 +0300

    Added some cases (not all) for Card

--- src/components/header/ui/Links.tsx
+++ src/components/header/ui/Links.tsx
@@ -16,10 +16,14 @@ export const Links = () => {
   }, [location])
 
   return (
-    <ul className='hidden xl:flex xl:gap-[60px]'>
+    <ul
+      className='hidden xl:flex xl:gap-[60px]'
+      data-testid='links'
+    >
       {links.map((item, index) => (
         <li key={index}>
           <Link
+            data-testid={active === item.href ? 'active-link' : 'link'}
             className={cn(
               'relative after:absolute after:-bottom-[0.5px] after:left-0 after:h-[0.5px] after:w-0 after:bg-primary-light after:transition-all after:duration-300 hover:after:w-full group-hover:after:w-full',
               active === item.href && 'border-b border-b-black'
