85c238c1eb1afc530717b2db8d3e0a4869c0c4f1
unknown <d.goncharenko.man@gmail.com>
2024-08-22 14:36:38 +0300

    Added test cases for Ingredients

---
+++ src/components/productCard/ui/Ingredients.test.tsx
@@ -0,0 +1,88 @@
+import { fireEvent, render } from '@testing-library/react'
+import { describe, expect, test, vi } from 'vitest'
+import { initIngredients } from '@/helpers/productCard.helpers'
+import { Ingredient, Ingredients } from './Ingredients'
+
+describe('Ingredients', () => {
+  test('should render', () => {
+    const { getByTestId } = render(
+      <Ingredients
+        handleChangeQuantity={vi.fn()}
+        ingredientsQuantity={{}}
+        ingredients={[]}
+      />
+    )
+
+    expect(getByTestId('ingredients-container')).toBeInTheDocument()
+  })
+
+  test("should render all ingredients' attributes correctly", () => {
+    const testIngredient = { name: 'борошно', mass: 150, price: 20 }
+    const { getByTestId } = render(
+      <Ingredient
+        handleChangeQuantity={vi.fn()}
+        ingredientsQuantity={initIngredients([testIngredient])}
+        ingredient={testIngredient}
+      />
+    )
+
+    expect(getByTestId('ingredient-name-mobile')).toHaveTextContent(
+      testIngredient.name
+    )
+    expect(getByTestId('ingredient-name-laptop')).toHaveTextContent(
+      testIngredient.name
+    )
+
+    expect(getByTestId('ingredient-image')).toHaveAttribute(
+      'alt',
+      testIngredient.name
+    )
+
+    expect(getByTestId('ingredient-price-mobile')).toHaveTextContent(
+      `0 х ${testIngredient.price}₴`
+    )
+    expect(getByTestId('ingredient-price-laptop').textContent).toEqual(
+      `0 х ${testIngredient.price}₴`
+    )
+
+    expect(getByTestId('ingredient-mass')).toHaveTextContent(
+      `${testIngredient.mass} ${testIngredient.mass < 1000 ? 'г' : 'кг'}`
+    )
+
+    expect(getByTestId('ingredient-count-mobile')).toHaveTextContent('0')
+    expect(getByTestId('ingredient-count-laptop')).toHaveTextContent('0')
+  })
+
+  test('should handle change quantity correctly', () => {
+    const handler = vi.fn()
+
+    const testIngredient = { name: 'борошно', mass: 150, price: 20 }
+    const { getByTestId } = render(
+      <Ingredient
+        handleChangeQuantity={handler}
+        ingredientsQuantity={initIngredients([testIngredient])}
+        ingredient={testIngredient}
+      />
+    )
+
+    // Test decreasing the quantity
+    const decreaseButtonMobile = getByTestId('decrease-btn-mobile')
+    const decreaseButtonLaptop = getByTestId('decrease-btn-laptop')
+
+    fireEvent.click(decreaseButtonMobile)
+    fireEvent.click(decreaseButtonLaptop)
+
+    expect(handler).toHaveBeenCalledTimes(2)
+    expect(handler).toHaveBeenCalledWith('DECREASE', testIngredient.name)
+
+    // Test increasing the quantity
+    const increaseButtonMobile = getByTestId('increase-btn-mobile')
+    const increaseButtonLaptop = getByTestId('increase-btn-laptop')
+
+    fireEvent.click(increaseButtonMobile)
+    fireEvent.click(increaseButtonLaptop)
+
+    expect(handler).toHaveBeenCalledTimes(4)
+    expect(handler).toHaveBeenCalledWith('INCREASE', testIngredient.name)
+  })
+})
--- src/components/productCard/ui/Ingredients.tsx
+++ src/components/productCard/ui/Ingredients.tsx
@@ -5,7 +5,7 @@ import { formatMass } from '@/helpers/newDishes.helpers'
 import classes from '../classes.module.css'
 import { Dish } from '@/data/menu.data'
 
-type Props = {
+export type Props = {
   ingredients: Dish['ingredients']
   ingredientsQuantity: { [P in string]: { count: number; price: number } }
   handleChangeQuantity: (
@@ -20,89 +20,167 @@ export const Ingredients = ({
   handleChangeQuantity
 }: Props) => {
   return (
-    <div className={classes.ingredients}>
-      {ingredients.map((ingr, index) => {
-        return (
-          <div
-            key={index}
-            className='bg-white p-1 rounded-lg'
+    <div
+      className={classes.ingredients}
+      data-testid='ingredients-container'
+    >
+      {ingredients.map(ingr => (
+        <Ingredient
+          handleChangeQuantity={handleChangeQuantity}
+          ingredient={ingr}
+          ingredientsQuantity={ingredientsQuantity}
+        />
+      ))}
+    </div>
+  )
+}
+
+export type ItemProps = {
+  ingredientsQuantity: Props['ingredientsQuantity']
+  handleChangeQuantity: Props['handleChangeQuantity']
+  ingredient: Props['ingredients'][number]
+}
+
+// Be aware that each section of an ingredient has it's own markup for each screen size
+// Look at data-testids for navigation:
+
+// For ingredient's name attribute there are: ingredient-name-mobile, ingredient-name-laptop
+// For ingredient's image there is: ingredient-image
+// For ingredient's price there are: ingredient-price-laptop, ingredient-price-mobile
+// For ingredient's mass there is: ingredient-mass
+// For ingredient's count there are: ingredient-count-mobile, ingredient-count-laptop
+// For increase quantity button: increase-btn-laptop, increase-btn-mobile
+// For decrease quantity button: decrease-btn-laptop, decrease-btn-mobile
+
+export const Ingredient = ({
+  handleChangeQuantity,
+  ingredient: ingr,
+  ingredientsQuantity
+}: ItemProps) => {
+  return (
+    <div className='bg-white p-1 rounded-lg'>
+      {/* Ingredient's price for smaller screens */}
+      <h3
+        className='font-semibold capitalize mb-2 xl:hidden'
+        data-testid='ingredient-name-mobile'
+      >
+        {ingr.name}
+      </h3>
+      {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
+      <div className='flex justify-center gap-1 xl:gap-3 items-center'>
+        <div className='relative xl:flex xl:items-center xl:flex-col h-16 w-16 xl:h-[68px] xl:w-[68px]'>
+          {/* TODO: add images for each ingredient, and don't forget to update the specific test case for this */}
+          <Image
+            src={img.src}
+            fill
+            alt={ingr.name}
+            className='rounded-lg -mt-3'
+            data-testid='ingredient-image'
+          />
+          {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
+
+          {/* Ingredient's price for big screens */}
+          {!!ingredientsQuantity[ingr.name]?.count != null && (
+            <span
+              className='hidden xl:block text-xs mt-[100%]'
+              data-testid='ingredient-price-laptop'
+            >
+              {ingredientsQuantity[ingr.name].count} х {ingr.price}₴
+            </span>
+          )}
+          {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
+        </div>
+        <div className='text-xs'>
+          {/* Ingredient's name for big screens */}
+          <h3
+            className='font-semibold capitalize mb-2 hidden xl:block'
+            data-testid='ingredient-name-laptop'
           >
-            <h3 className='font-semibold capitalize mb-2 xl:hidden'>
-              {ingr.name}
-            </h3>
-            <div className='flex justify-center gap-1 xl:gap-3 items-center'>
-              <div className='relative xl:flex xl:items-center xl:flex-col h-16 w-16 xl:h-[68px] xl:w-[68px]'>
-                <Image
-                  src={img.src}
-                  fill
-                  alt={ingr.name}
-                  className='rounded-lg -mt-3'
-                />
-                {!!ingredientsQuantity[ingr.name]?.count && (
-                  <span className='hidden xl:block text-xs mt-[100%]'>
-                    {ingredientsQuantity[ingr.name].count} х {ingr.price}₴
-                  </span>
-                )}
-              </div>
-              <div className='text-xs'>
-                <h3 className='font-semibold capitalize mb-2 hidden xl:block'>
-                  {ingr.name}
-                </h3>
-                <span className='block whitespace-nowrap'>
-                  {formatMass(ingr.mass)}
-                </span>
-                {!!ingredientsQuantity[ingr.name]?.count ? (
-                  <span className='xl:hidden text-xs'>
-                    {ingredientsQuantity[ingr.name].count} х {ingr.price}₴
-                  </span>
-                ) : (
-                  <span className='xl:hidden text-xs'>0 х {ingr.price}₴</span>
-                )}
+            {ingr.name}
+          </h3>
+          {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
 
-                <div className='gap-2 items-center justify-center mt-3 hidden xl:flex xl:mt-7'>
-                  <Button
-                    className='border border-black border-opacity-5 p-0 w-8 h-8 rounded-lg'
-                    onClick={() => handleChangeQuantity('DECREASE', ingr.name)}
-                  >
-                    -
-                  </Button>
-                  {ingredientsQuantity[ingr.name] != null && (
-                    <span className='flex items-center justify-center border border-black border-opacity-10 p-0 w-8 h-8 rounded-lg'>
-                      {ingredientsQuantity[ingr.name].count}
-                    </span>
-                  )}
+          <span
+            className='block whitespace-nowrap'
+            data-testid='ingredient-mass'
+          >
+            {formatMass(ingr.mass)}
+          </span>
 
-                  <Button
-                    className='border border-black border-opacity-5 p-0 w-8 h-8 rounded-lg'
-                    onClick={() => handleChangeQuantity('INCREASE', ingr.name)}
-                  >
-                    +
-                  </Button>
-                </div>
-              </div>
-            </div>
-            <div className='flex gap-2 items-center justify-center mt-3 xl:hidden'>
-              <Button
-                className='border border-black border-opacity-5 p-0 w-8 h-8 rounded-lg'
-                onClick={() => handleChangeQuantity('DECREASE', ingr.name)}
+          {/* Ingredient's price for mobile screens */}
+          {!!ingredientsQuantity[ingr.name]?.count != null ? (
+            <span
+              className='xl:hidden text-xs'
+              data-testid='ingredient-price-mobile'
+            >
+              {ingredientsQuantity[ingr.name].count} х {ingr.price}₴
+            </span>
+          ) : (
+            <span
+              className='xl:hidden text-xs'
+              data-testid='ingredient-price-mobile'
+            >
+              0 х {ingr.price}₴
+            </span>
+          )}
+          {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
+
+          {/* Change quantity buttons for big screens */}
+          <div className='gap-2 items-center justify-center mt-3 hidden xl:flex xl:mt-7'>
+            <Button
+              className='border border-black border-opacity-5 p-0 w-8 h-8 rounded-lg'
+              onClick={() => handleChangeQuantity('DECREASE', ingr.name)}
+              data-testid='decrease-btn-laptop'
+            >
+              -
+            </Button>
+            {ingredientsQuantity[ingr.name] != null && (
+              <span
+                className='flex items-center justify-center border border-black border-opacity-10 p-0 w-8 h-8 rounded-lg'
+                data-testid='ingredient-count-laptop'
               >
-                -
-              </Button>
-              {ingredientsQuantity[ingr.name]?.count && (
-                <span className='flex items-center justify-center border border-black border-opacity-10 p-0 w-8 h-8 rounded-lg'>
-                  {ingredientsQuantity[ingr.name].count}
-                </span>
-              )}
-              <Button
-                className='border border-black border-opacity-5 p-0 w-8 h-8 rounded-lg'
-                onClick={() => handleChangeQuantity('INCREASE', ingr.name)}
-              >
-                +
-              </Button>
-            </div>
+                {ingredientsQuantity[ingr.name].count}
+              </span>
+            )}
+
+            <Button
+              className='border border-black border-opacity-5 p-0 w-8 h-8 rounded-lg'
+              onClick={() => handleChangeQuantity('INCREASE', ingr.name)}
+              data-testid='increase-btn-laptop'
+            >
+              +
+            </Button>
           </div>
-        )
-      })}
+          {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
+        </div>
+      </div>
+
+      {/* Change quantity buttons for smaller screens */}
+      <div className='flex gap-2 items-center justify-center mt-3 xl:hidden'>
+        <Button
+          className='border border-black border-opacity-5 p-0 w-8 h-8 rounded-lg'
+          onClick={() => handleChangeQuantity('DECREASE', ingr.name)}
+          data-testid='decrease-btn-mobile'
+        >
+          -
+        </Button>
+        {ingredientsQuantity[ingr.name]?.count != null && (
+          <span
+            className='flex items-center justify-center border border-black border-opacity-10 p-0 w-8 h-8 rounded-lg'
+            data-testid='ingredient-count-mobile'
+          >
+            {ingredientsQuantity[ingr.name].count}
+          </span>
+        )}
+        <Button
+          className='border border-black border-opacity-5 p-0 w-8 h-8 rounded-lg'
+          onClick={() => handleChangeQuantity('INCREASE', ingr.name)}
+          data-testid='increase-btn-mobile'
+        >
+          +
+        </Button>
+      </div>
+      {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
     </div>
   )
 }
