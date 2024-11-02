a9fe89985d92f34ad2976e53f00bb6d25bdc8856
zim89 <121241841+zim89@users.noreply.github.com>
2024-11-02 12:07:37 +0200

    add adaptive for menu-no-result

--- src/views/root/menu/ui/menu-no-results.tsx
+++ src/views/root/menu/ui/menu-no-results.tsx
@@ -4,8 +4,8 @@ import not_found_img from '@/shared/assets/images/filter-not-found.png'
 
 export const MenuNoResults = () => {
   return (
-    <div className='flex flex-col items-center gap-[49.16px] pt-10'>
-      <div className='relative h-[329.84px] w-[530px]'>
+    <div className='flex flex-col items-center gap-14 pt-[39px] md:pt-[27px] xl:gap-[49.16px] xl:pt-10'>
+      <div className='relative h-[180px] w-[288px] md:h-[244px] md:w-[391px] xl:h-[329.84px] xl:w-[530px]'>
         <Image
           src={not_found_img}
           alt='No results image'
@@ -14,8 +14,9 @@ export const MenuNoResults = () => {
           className='object-cover'
         />
       </div>
-      <p className='text-center text-2xl/[29.05px] text-primary-dark'>
-        Ми наразі працюємо над наповненням цього розділу!
+      <p className='text-center text-xl/[24.2px] text-primary-dark xl:text-2xl/[29.05px]'>
+        Ми наразі працюємо над наповненням цього <br className='md:hidden' />
+        розділу!
       </p>
     </div>
   )
