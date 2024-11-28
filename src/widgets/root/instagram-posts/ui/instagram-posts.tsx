49d1f1586362e6e7d352da414f3143e474b09558
zim89 <121241841+zim89@users.noreply.github.com>
2024-11-28 14:21:39 +0200

    Update instagram-posts.tsx
    hide slider navs from mobile

--- src/widgets/root/instagram-posts/ui/instagram-posts.tsx
+++ src/widgets/root/instagram-posts/ui/instagram-posts.tsx
@@ -18,25 +18,18 @@ export const InstagramPosts = () => {
   const { data, cursor } = usePosts()
   const [api, setApi] = useState<CarouselApi>()
   const [current, setCurrent] = useState(0)
-  const [count, setCount] = useState(0)
 
   useEffect(() => {
     if (!api) {
       return
     }
 
-    setCount(api.scrollSnapList().length)
     setCurrent(api.selectedScrollSnap() + 1)
 
     api.on('select', () => {
       setCurrent(api.selectedScrollSnap() + 1)
     })
   }, [api])
-
-  useEffect(() => {
-    console.log('[CURRENT]: ', current)
-    console.log('[COUNT]: ', count)
-  }, [count, current])
 
   return (
     <section className='section pt-10 md:pt-[72px]'>
@@ -84,7 +77,7 @@ export const InstagramPosts = () => {
               ))}
             </CarouselContent>
 
-            <div className='absolute -top-[73px] right-0 flex h-10 items-center gap-10 xl:top-0'>
+            <div className='absolute -top-[73px] right-0 hidden h-10 items-center gap-10 md:flex xl:top-0'>
               <CarouselPrevious className='static translate-x-0 translate-y-0' />
               <CarouselNext className='static translate-x-0 translate-y-0' />
             </div>
