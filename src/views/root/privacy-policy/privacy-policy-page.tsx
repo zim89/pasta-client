4a6a77b6f54ce1ca950f6191617d01915367253e
Dima Honcharenko <d.goncharenko.man@gmail.com>
2024-12-26 14:03:32 +0200

    Added privacy policy page

--- src/views/root/privacy-policy/privacy-policy-page.tsx
+++ src/views/root/privacy-policy/privacy-policy-page.tsx
@@ -1,7 +1,18 @@
+'use client'
+
+import { Viewer } from '@react-pdf-viewer/core'
+
+// Import the styles
+import '@react-pdf-viewer/core/lib/styles/index.css'
+
 export const PrivacyPolicyPage = () => {
   return (
-    <div className='page-wrap'>
-      <div className='container'>PrivacyPolicyPage</div>
+    <div className='page-wrap bg-white'>
+      <Viewer
+        defaultScale={1.3}
+        enableSmoothScroll
+        fileUrl='/files/polityka-konfidentsiynosti.pdf'
+      />
     </div>
   )
 }
