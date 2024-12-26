cc35c250e667f641fe6c94096c36728ffd60d1be
Dima Honcharenko <d.goncharenko.man@gmail.com>
2024-12-26 14:01:09 +0200

    Added public offer page

--- src/app/providers/root-providers.tsx
+++ src/app/providers/root-providers.tsx
@@ -2,6 +2,7 @@
 
 import { PropsWithChildren } from 'react'
 import { UnsavedChangesProvider } from '@/shared/context/root-unsaved-changes'
+import { Worker } from '@react-pdf-viewer/core'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
@@ -11,15 +12,17 @@ import { queryClient } from '@/shared/api'
 
 export const Providers = ({ children }: PropsWithChildren) => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <UnsavedChangesProvider>
-        <CartStoreProvider>
-          {children}
-          <Toaster />
-          <ReactQueryDevtools initialIsOpen={false} />
-        </CartStoreProvider>
-      </UnsavedChangesProvider>
-    </QueryClientProvider>
+    <Worker workerUrl='https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js'>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <UnsavedChangesProvider>
+          <CartStoreProvider>
+            {children}
+            <Toaster />
+            <ReactQueryDevtools initialIsOpen={false} />
+          </CartStoreProvider>
+        </UnsavedChangesProvider>
+      </QueryClientProvider>
+    </Worker>
   )
 }
